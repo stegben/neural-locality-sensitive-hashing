5abf755a6f36881d4c00c404ab84758a26746f1b
stegben <stegben.benjamin@gmail.com>
2019-10-28 14:59:20 +0800

    init code distance file

---
+++ nlsh/learning/distances.py
@@ -0,0 +1,13 @@
+# distance function for hash code
+import torch.nn.functional as F
+
+
+L2 = F.pairwise_distance
+
+
+def JSD_categorical(p1, p2):
+    pass
+
+
+def JSD_multivariate_bernoulli(p1, p2):
+    pass
