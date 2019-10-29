1951f5a09c75770979ef6e6de5dbd2213bdbb616
stegben <stegben.benjamin@gmail.com>
2019-10-29 14:27:30 +0800

    more test for JSD_divergence

--- nlsh/learning/tests/test_distances.py
+++ nlsh/learning/tests/test_distances.py
@@ -5,10 +5,10 @@ from ..distances import JSD_categorical
 
 
 def test_JSD_categorical():
-    p = torch.Tensor([[0.1, 0.9], [0.1, 0.9]])
-    q = torch.Tensor([[0.5, 0.5], [0.1, 0.9]])
+    p = torch.Tensor([[0.1, 0.9], [0.1, 0.9], [1., 0.]])
+    q = torch.Tensor([[0.5, 0.5], [0.1, 0.9], [0., 1.]])
     divergence = JSD_categorical(p, q)
     np.testing.assert_array_almost_equal(
         divergence.numpy(),
-        np.array([0.101749, 0.]),
+        np.array([0.101749, 0., 0.693147]),
     )
