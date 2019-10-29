84a7ba1800ffa9694c175b4f35b1036466bbb1e5
stegben <stegben.benjamin@gmail.com>
2019-10-29 14:24:06 +0800

    impl JS divergence of categorical distribution

--- nlsh/learning/distances.py
+++ nlsh/learning/distances.py
@@ -1,12 +1,27 @@
 # distance function for hash code
 import torch.nn.functional as F
+from torch.distributions import Categorical, kl_divergence
 
 
 L2 = F.pairwise_distance
 
 
-def JSD_categorical(p1, p2):
-    pass
+def JSD_categorical(p, q):
+    """
+    p: (n, k)
+    q: (n, k)
+
+    outputs d: (n)
+    """
+    m = (p + q) / 2
+
+    p = Categorical(probs=p)
+    q = Categorical(probs=q)
+    m = Categorical(probs=m)
+
+    kl_pm = kl_divergence(p, m)
+    kl_qm = kl_divergence(q, m)
+    return (kl_pm + kl_qm) / 2
 
 
 def JSD_multivariate_bernoulli(p1, p2):
---
+++ nlsh/learning/tests/test_distances.py
@@ -0,0 +1,14 @@
+import numpy as np
+import torch
+
+from ..distances import JSD_categorical
+
+
+def test_JSD_categorical():
+    p = torch.Tensor([[0.1, 0.9], [0.1, 0.9]])
+    q = torch.Tensor([[0.5, 0.5], [0.1, 0.9]])
+    divergence = JSD_categorical(p, q)
+    np.testing.assert_array_almost_equal(
+        divergence.numpy(),
+        np.array([0.101749, 0.]),
+    )
