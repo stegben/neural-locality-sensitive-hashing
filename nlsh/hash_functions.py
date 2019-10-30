f9f75848577e72c7fda19aeee8c34816a067d565
stegben <stegben.benjamin@gmail.com>
2019-10-30 13:24:40 +0800

    refactor hash_function

--- nlsh/hash_functions.py
+++ nlsh/hash_functions.py
@@ -1,23 +1,63 @@
 # learnable hash functions
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
-class MultivariateBernoulli(nn.Module):
+class MultivariateBernoulli:
 
-    def __init__(self, input_dim: int, hash_size: int):
-        super().__init__()
-        self.fc1 = nn.Linear(input_dim, 256)
-        self.fc2 = nn.Linear(256, 256)
-        self.output_layer = nn.Linear(256, hash_size)
+    class _Hasher(nn.Module):
 
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.sigmoid(self.output_layer(x))
-        return x
+        def __init__(self, encoder, hash_size):
+            super().__init__()
+            self._encoder = encoder
+            self.output_layer = nn.Linear(encoder.output_dim, hash_size)
 
-    def distance(self,):
+        def forward(self, x):
+            x = self._encoder(x)
+            x = F.sigmoid(self.output_layer(x))
+            return x
+
+    def __init__(self, encoder, hash_size, distance_func):
+        self._encoder = encoder
+        self._hash_size = hash_size
+        self._distance_func = distance_func
+
+        self._hasher = self._Hasher(self._encoder, self._hash_size)
+
+    def predict(self, x):
+        return self._hasher(x)
+
+    def distance(self, y1, y2):
+        return self._distance_func(y1, y2)
+
+    def parameters(self):
+        return self._hasher.parameters()
+
+    def save(self, base_name):
+        scripted_model_cpu = torch.jit.script(self._hasher.cpu())
+        torch.jit.save(scripted_model_cpu, base_name+"_cpu.pt")
+        scripted_model_gpu = torch.jit.script(self._hasher.cuda())
+        torch.jit.save(scripted_model_gpu, base_name+"_gpu.pt")
+
+    def _binarr_to_int(self, binarr):
+        out = 0
+        for bit in binarr:
+            out = (out << 1) | bit
+        return out
+
+    def hash(self, query_vectors):
+        probs = self._hasher(query_vectors)
+        codes = (probs > 0.5).tolist()
+        hash_results = []
+        for binarr in codes:
+            hash_results.append(self._binarr_to_int(binarr))
+        return hash_results
+
+    def hash_sample(self, query_vectors, sample_size=10):
+        # probs = self._hasher(query_vectors)
+        # TODO: sampling hash. The larger the sample size,
+        # the better recall should be
         pass
 
 
@@ -39,3 +79,6 @@ class Categorical(nn.Module):
     def hash(self, x):
         prob = self.forward(x)
         return prob.argmax(axis=1)
+
+    def distance(self, y1, y2):
+        return self._distance_func(y1, y2)
