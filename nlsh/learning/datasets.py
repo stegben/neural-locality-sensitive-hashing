68b298897b999203add536cb60eae9653f34ee1d
stegben <stegben.benjamin@gmail.com>
2019-11-02 03:45:14 +0800

    custom batch generator, much faster

--- nlsh/learning/datasets.py
+++ nlsh/learning/datasets.py
@@ -1,4 +1,6 @@
-import random
+from random import randint, random
+
+import numpy as np
 from torch.utils.data import Dataset
 
 
@@ -43,18 +45,40 @@ class KNearestNeighborTriplet(Dataset):
         return self.n_candidates
 
     def __getitem__(self, idx: int):
+        idx = int(idx)
         anchor = self._candidate_vectors[idx, :]
 
         # positive sample from _candidate_self_knn
-        pre_v1_idx = random.randint(0, self.k - 1)
-        v1_idx = self._candidate_self_knn[idx, pre_v1_idx]
+        pre_v1_idx = randint(0, self.k - 1)
+        v1_idx = int(self._candidate_self_knn[idx, pre_v1_idx])
 
         # negative sample randomly select from the dataset
-        v2_idx = random.randint(0, self.n_candidates - 1)
+        v2_idx = randint(0, self.n_candidates - 1)
 
         v1 = self._candidate_vectors[v1_idx, :]
         v2 = self._candidate_vectors[v2_idx, :]
         return anchor, v1, v2
+
+    def batch_generator(self, batch_size, shuffle=False):
+        n_batches = len(self) // batch_size
+
+        anchor_idxs = np.arange(len(self))
+        if shuffle:
+            np.random.shuffle(anchor_idxs)
+
+        for idx in range(n_batches):
+            start = idx * batch_size
+            end = (idx + 1) * batch_size
+
+            anchor = self._candidate_vectors[anchor_idxs[start:end], :]
+
+            train_k = self._candidate_self_knn.shape[1]
+            knn_idxs = self._candidate_self_knn[anchor_idxs[start:end], np.random.randint(0, train_k, (batch_size,))]
+            positive = self._candidate_vectors[knn_idxs, :]
+
+            random_idxs = np.random.randint(0, len(self), (batch_size,))
+            negative = self._candidate_vectors[random_idxs, :]
+            yield anchor, positive, negative
 
 
 class KNearestNeighborSiamese(Dataset):
@@ -79,14 +103,14 @@ class KNearestNeighborSiamese(Dataset):
     def __getitem__(self, idx: int):
         anchor = self._candidate_vectors[idx, :]
 
-        if random.random() < self._positive_rate:
+        if random() < self._positive_rate:
             # positive sample from _candidate_self_knn
-            pre_v_idx = random.randint(0, self.k - 1)
+            pre_v_idx = randint(0, self.k - 1)
             v_idx = self._candidate_self_knn[idx, pre_v_idx]
             label = 1
         else:
             # negative sample randomly select from the dataset
-            v_idx = random.randint(0, self.n_candidates - 1)
+            v_idx = randint(0, self.n_candidates - 1)
             label = 0
 
         v = self._candidate_vectors[v_idx, :]
