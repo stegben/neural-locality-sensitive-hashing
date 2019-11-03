e995456316af88252a718d1e340bdf999196d312
stegben <stegben.benjamin@gmail.com>
2019-11-04 01:49:20 +0800

    setup comet logger in main

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 from encoders import TwoLayer256Relu
 from nlsh.hashings import MultivariateBernoulli, Categorical
 from nlsh.data import Glove
-from nlsh.loggers import TensorboardX
+from nlsh.loggers import TensorboardX, CometML
 from nlsh.trainers import TripletTrainer
 
 from nlsh.learning.datasets import KNearestNeighborTriplet
@@ -19,6 +19,10 @@ load_dotenv()
 
 LOG_BASE_DIR = os.environ["NLSH_TENSORBOARD_LOG_DIR"]
 MODEL_SAVE_DIR = os.environ["NLSH_MODEL_SAVE_DIR"]
+
+COMET_API_KEY = os.environ["NLSH_COMET_API_KEY"]
+COMET_PROJECT_NAME = os.environ["NLSH_COMET_PROJECT_NAME"]
+COMET_WORKSPACE = os.environ["NLSH_COMET_WORKSPACE"]
 
 
 def nlsh_argparse():
@@ -86,7 +90,13 @@ def main():
     RUN_TIME = datetime.now().strftime("%Y%m%d-%H%M%S")
     if args.log_tag is None:
         RUN_NAME = f"{int(2**hash_size)}_triplet_{RUN_TIME}"
-    logger = TensorboardX(f"{LOG_BASE_DIR}/{RUN_NAME}", RUN_NAME)
+    # logger = TensorboardX(f"{LOG_BASE_DIR}/{RUN_NAME}", RUN_NAME)
+    logger = CometML(
+        api_key=COMET_API_KEY,
+        project_name=COMET_PROJECT_NAME,
+        workspace=COMET_WORKSPACE,
+        debug=False,
+    )
     logger.meta(params={
         'hash_size': hash_size,
         'lambda1': lambda1,
