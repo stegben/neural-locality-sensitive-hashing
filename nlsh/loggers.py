c802bfd924cf5d5630c0d17452e2891967efc9a5
stegben <stegben.benjamin@gmail.com>
2019-11-04 01:49:02 +0800

    impl CometML logger

--- nlsh/loggers.py
+++ nlsh/loggers.py
@@ -1,5 +1,5 @@
 from tensorboardX import SummaryWriter
-
+from comet_ml import Experiment
 
 class NullLogger:
 
@@ -30,24 +30,39 @@ class TensorboardX:
     def meta(self, params):
         self._writer.add_hparams(hparam_dict=params, metric_dict={})
 
-    def log(self, name, loss, step):
-        self._writer.add_scalar(name, loss, step)
+    def log(self, name, value, step):
+        self._writer.add_scalar(name, value, step)
 
 
 class CometML:
 
-    def __init__(self):
-        pass
+    def __init__(self, api_key, project_name, workspace, debug=True):
+        self._exp = Experiment(
+            api_key=api_key,
+            project_name=project_name,
+            workspace=workspace,
+            disabled=debug,
+        )
+        if not self._exp.alive:
+            raise RuntimeError("Cannot connect to Comet ML")
+        self._exp.disable_mp()
 
     @property
     def run_name(self):
-        return "Null"
+        return self._exp.get_key()
 
-    def meta(self, *args, **kwargs):
-        pass
+    def args(self, arg_text):
+        self._exp.log_parameter("cmd args", arg_text)
 
-    def log(self, name, loss, step):
-        pass
+    def meta(self, params):
+        self._exp.log_parameters(params)
+
+    def log(self, name, value, step):
+        self._exp.log_metric(
+            name=name,
+            value=value,
+            step=step,
+        )
 
 
 class WanDB:
@@ -62,5 +77,5 @@ class WanDB:
     def meta(self, *args, **kwargs):
         pass
 
-    def log(self, name, loss, step):
+    def log(self, name, value, step):
         pass
