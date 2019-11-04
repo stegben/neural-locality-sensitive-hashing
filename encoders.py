e2ad7bafa671cb396585d31d49307ca689881829
stegben <stegben.benjamin@gmail.com>
2019-11-04 22:28:26 +0800

    add a more flexible encoder

--- encoders.py
+++ encoders.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -16,3 +18,25 @@ class TwoLayer256Relu(nn.Module):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         return x
+
+
+class MultiLayerRelu(nn.Module):
+
+    def __init__(self, input_dim, hidden_dims: List[int]):
+        super().__init__()
+        self._input_dim = input_dim
+        self._hidden_dims = hidden_dims
+
+        self.output_dims = hidden_dims[-1]
+
+        self.fcs = []
+        prev_dim = input_dim
+        for dim in hidden_dims:
+            self.fcs.append(nn.Linear(prev_dim, dim))
+            prev_dim = dim
+
+    def forward(self, x):
+        output = x
+        for fc in self.fcs:
+            output = F.relu(fc(output))
+        return output
