be57125c6c467fd7f07817f4e3bb5d23564e8ef0
stegben <stegben.benjamin@gmail.com>
2019-11-21 15:38:38 +0800

    refactor main.py

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ from dotenv import load_dotenv
 from encoders import MultiLayerRelu
 from nlsh.hashings import MultivariateBernoulli, Categorical
 from nlsh.data import Glove
-from nlsh.loggers import TensorboardX, CometML
-from nlsh.trainers import TripletTrainer
+from nlsh.loggers import TensorboardX, CometML, NullLogger
+from nlsh.trainers import TripletTrainer, SiameseTrainer
 
 from nlsh.learning.datasets import KNearestNeighborTriplet
 from nlsh.learning.distances import (
     L2,
     JSD_categorical,
+    KL_multivariate_bernoulli,
 )
 
 load_dotenv()
@@ -60,14 +61,107 @@ def hashing_type(value):
 
 def get_hashing_from_args(args, enc):
     hashing_type = args.hashing_type
+    distance_type = args.distance_type
+
     if hashing_type == "Categorical":
         hash_size = int(2 ** args.hash_size)
-        return Categorical(enc, hash_size, JSD_categorical)
+        if distance_type == "L2":
+            return Categorical(enc, hash_size, L2)
+        elif distance_type == "JS":
+            return Categorical(enc, hash_size, JSD_categorical)
+        else:
+            raise RuntimeError(f"{distance_type} is not valid for {hashing_type}")
+
     elif hashing_type == "MultivariateBernoulli":
         hash_size = args.hash_size
-        return MultivariateBernoulli(enc, hash_size, L2)
+        if distance_type == "L2":
+            return MultivariateBernoulli(enc, hash_size, L2)
+        elif distance_type == "KL":
+            return MultivariateBernoulli(enc, hash_size, KL_multivariate_bernoulli)
+        else:
+            raise RuntimeError(f"{distance_type} is not valid for {hashing_type}")
+
     else:
         raise RuntimeError(f"{hashing_type} is not a valid hashing type")
+
+
+def get_logger_from_args(args):
+    if args.debug:
+        logger = NullLogger()
+    else:
+        if args.logger_type == "cometml":
+            log_tags = args.log_tags
+            log_tags = log_tags.split(",") if log_tags is not None else None
+            logger = CometML(
+                api_key=COMET_API_KEY,
+                project_name=COMET_PROJECT_NAME,
+                workspace=COMET_WORKSPACE,
+                debug=args.debug,
+                tags=log_tags,
+            )
+        elif args.logger_type == "tensorboard":
+            run_time = datetime.now().strftime("%Y%m%d-%H%M%S")
+            run_name = f"{int(2**args.hash_size)}_triplet_{run_time}"
+            logger = TensorboardX(f"{LOG_BASE_DIR}/{run_name}", run_name)
+        else:
+            raise RuntimeError(f"{args.logger_type} is not a valid logger type")
+
+    logger.meta(params={
+        'k': args.k,
+
+        # hash function related
+        'hash_size': args.hash_size,
+        'encoder_structure': args.encoder_structure,
+        'distance_type': args.distance_type,
+
+        # data related
+        'data_id': args.data_id,
+
+        # learner related
+        'lambda1': args.lambda1,
+        'learner_type': args.learner_type,
+        'triplet_margin': args.triplet_margin,
+        'siamese_positive_margin': args.siamese_positive_margin,
+        'siamese_negative_margin': args.siamese_negative_margin,
+        'siamese_positive_rate': args.siamese_positive_rate,
+
+        # fitting related
+        'learning_rate': args.learning_rate,
+        'batch_size': args.batch_size,
+    })
+    logger.args(' '.join(sys.argv[1:]))
+    return logger
+
+
+def get_learner_from_args(args, hashing, data, logger):
+    if args.learner_type == "triplet":
+        lambda1 = args.lambda1
+        margin = args.triplet_margin
+        learner = TripletTrainer(
+            hashing,
+            data,
+            MODEL_SAVE_DIR,
+            logger=logger,
+            lambda1=lambda1,
+            margin=margin,
+        )
+    elif args.learner_type == "siamese":
+        lambda1 = args.lambda1
+        positive_margin = args.siamese_positive_margin
+        negative_margin = args.siamese_negative_margin
+        positive_rate = args.siamese_positive_rate
+
+        learner = SiameseTrainer(
+            hashing,
+            data,
+            MODEL_SAVE_DIR,
+            logger=logger,
+            lambda1=lambda1,
+            positive_margin=positive_margin,
+            negative_margin=negative_margin,
+            positive_rate=positive_rate,
+        )
+    return learner
 
 
 def nlsh_argparse():
@@ -94,6 +188,14 @@ def nlsh_argparse():
         "--hashing_type",
         type=str,
         default='MultivariateBernoulli',
+        choices=("MultivariateBernoulli", "Categorical"),
+    )
+    parser.add_argument(
+        "-dt",
+        "--distance_type",
+        type=str,
+        default='L2',
+        choices=("L2", "JS", "KL"),
     )
     parser.add_argument(
         "--data_id",
@@ -101,21 +203,43 @@ def nlsh_argparse():
         choices=("glove_25", "glove_50", "glove_100", "glove_200",),
     )
     parser.add_argument(
+        "--logger_type",
+        type=str,
+        choices=("tensorboard", "cometml"),
+    )
+    parser.add_argument(
         "--log_tags",
         type=str,
         default=None,
     )
     parser.add_argument(
+        "--learner_type",
+        type=str,
+        choices=("triplet", "siamese"),
+    )
+    parser.add_argument(
         "-tm",
         "--triplet_margin",
         type=float,
-        default=10.0,
+        default=None,
     )
     parser.add_argument(
-        "-pr",
-        "--positive_rate",
+        "-spm",
+        "--siamese_positive_margin",
         type=float,
-        default=0.1,
+        default=None,
+    )
+    parser.add_argument(
+        "-snm",
+        "--siamese_negative_margin",
+        type=float,
+        default=None,
+    )
+    parser.add_argument(
+        "-spr",
+        "--siamese_positive_rate",
+        type=float,
+        default=None,
     )
     parser.add_argument(
         "--lambda1",
@@ -134,6 +258,10 @@ def nlsh_argparse():
         type=float,
         default=3e-4,
     )
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+    )
     return parser
 
 
@@ -143,59 +271,20 @@ def main():
 
     # hyper params
     k = args.k
-    hash_size = args.hash_size
-    encoder_structure = args.encoder_structure
-    lambda1 = args.lambda1
-    triplet_margin = args.triplet_margin
-    positive_rate = args.positive_rate
     learning_rate = args.learning_rate
     batch_size = args.batch_size
-    log_tags = args.log_tags
-    log_tags = log_tags.split(",") if log_tags is not None else None
 
     data = get_data_by_id(args.data_id)
     data.load()
     enc = MultiLayerRelu(
         input_dim=data.dim,
-        hidden_dims=encoder_structure,
+        hidden_dims=args.encoder_structure,
     ).cuda()
     hashing = get_hashing_from_args(args, enc)
+    logger = get_logger_from_args(args)
+    learner = get_learner_from_args(args, hashing, data, logger)
 
-    RUN_TIME = datetime.now().strftime("%Y%m%d-%H%M%S")
-    # RUN_NAME = f"{int(2**hash_size)}_triplet_{RUN_TIME}"
-    # logger = TensorboardX(f"{LOG_BASE_DIR}/{RUN_NAME}", RUN_NAME)
-    logger = CometML(
-        api_key=COMET_API_KEY,
-        project_name=COMET_PROJECT_NAME,
-        workspace=COMET_WORKSPACE,
-        debug=False,
-        tags=log_tags,
-    )
-    logger.meta(params={
-        'hash_size': hash_size,
-        'encoder_structure': encoder_structure,
-        'lambda1': lambda1,
-        'triplet_margin': triplet_margin,
-        'positive_rate': positive_rate,
-        'learning_rate': learning_rate,
-        'batch_size': batch_size,
-        'data': "glove 25",
-        'k': k,
-        'code_distance': "2-norm",
-        'data_id': args.data_id,
-    })
-    logger.args(' '.join(sys.argv[1:]))
-    nlsh = TripletTrainer(
-        hashing,
-        data,
-        MODEL_SAVE_DIR,
-        logger=logger,
-        lambda1=lambda1,
-        triplet_margin=triplet_margin,
-        positive_rate=positive_rate,
-    )
-
-    nlsh.fit(
+    learner.fit(
         K=k,
         batch_size=batch_size,
         learning_rate=learning_rate,
