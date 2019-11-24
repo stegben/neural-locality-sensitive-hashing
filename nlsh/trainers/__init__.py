b5860e97686ae4cd38e5d0b08a75d4283a5fcd9a
stegben <stegben.benjamin@gmail.com>
2019-11-24 15:13:52 +0800

    create new folder for trainer to refactor

---
+++ nlsh/trainers/__init__.py
@@ -0,0 +1,10 @@
+from .siamese import SiameseTrainer
+from .triplet import TripletTrainer
+
+# TODO: new idea
+# from .vqvae import VQVAE
+
+# TODO: reporduce
+# from .kmeans import KMeansTrainer
+# from .kahip_classify import KaHipClassifer
+# from .product_quantization import ProductQuantization
