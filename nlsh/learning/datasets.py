fcd6afef09ed583b55e41dfedcfac344d0a382d3
stegben <stegben.benjamin@gmail.com>
2019-12-03 14:02:20 +0800

    add KNearestNeighborLocallySiamese

--- nlsh/learning/datasets.py
+++ nlsh/learning/datasets.py
@@ -100,8 +100,7 @@ class KNearestNeighborTriplet(Dataset):
 
             anchor = self._candidate_vectors[anchor_idxs[start:end], :]
 
-            train_k = self._candidate_self_knn.shape[1]
-            knn_idxs = self._candidate_self_knn[anchor_idxs[start:end], np.random.randint(0, train_k, (batch_size,))]
+            knn_idxs = self._candidate_self_knn[anchor_idxs[start:end], np.random.randint(0, self.k, (batch_size,))]
             positive = self._candidate_vectors[knn_idxs, :]
 
             random_idxs = np.random.randint(0, len(self), (batch_size,))
@@ -164,10 +163,76 @@ class KNearestNeighborSiamese(Dataset):
 
             anchor = self._candidate_vectors[anchor_idxs[start:end], :]
 
-            train_k = self._candidate_self_knn.shape[1]
-            ramdomly_selected_positive_idx = np.random.randint(0, train_k, (batch_size,))
+            ramdomly_selected_positive_idx = np.random.randint(0, self.k, (batch_size,))
             positive_idxs = self._candidate_self_knn[anchor_idxs[start:end], ramdomly_selected_positive_idx]
             negative_idxs = negative_idx_all[start:end]
+            label = label_all_np[start:end]
+            label_output = label_all[start:end]
+            other_idx = positive_idxs * label + negative_idxs * (1 - label)
+            other = self._candidate_vectors[other_idx, :]
+            yield anchor, other, label_output
+
+
+class KNearestNeighborLocallySiamese(Dataset):
+
+    def __init__(
+            self,
+            candidate_vectors,
+            candidate_self_knn,
+            inner_k=None,
+            outer_k=None,
+            positive_rate=0.1,
+        ):
+        self._candidate_vectors = candidate_vectors
+        self._candidate_self_knn = candidate_self_knn
+        self._positive_rate = positive_rate
+
+        self._inner_k = inner_k or candidate_self_knn.shape[1] // 2
+        self._outer_k = outer_k or candidate_self_knn.shape[1]
+        if self._outer_k <= self._inner_k:
+            raise ValueError(f"Outer K (got {self._outer_k}) should be larger than inner K (got {self._inner_k}).")
+        self.n_candidates = self._candidate_vectors.shape[0]
+
+    def __len__(self):
+        return self.n_candidates
+
+    def __getitem__(self, idx: int):
+        anchor = self._candidate_vectors[idx, :]
+
+        if random() < self._positive_rate:
+            # positive sample from _candidate_self_knn
+            pre_v_idx = randint(0, self.k - 1)
+            v_idx = self._candidate_self_knn[idx, pre_v_idx]
+            label = 1
+        else:
+            # negative sample randomly select from the dataset
+            v_idx = randint(0, self.n_candidates - 1)
+            label = 0
+
+        v = self._candidate_vectors[v_idx, :]
+        return anchor, v, label
+
+
+    def batch_generator(self, batch_size, shuffle=False):
+        n_batches = len(self) // batch_size
+
+        label_all_np = np.random.random((len(self),)) < self._positive_rate
+        label_all = torch.from_numpy(label_all_np).long().cuda()
+
+        anchor_idxs = np.arange(len(self))
+        if shuffle:
+            np.random.shuffle(anchor_idxs)
+
+        for idx in range(n_batches):
+            start = idx * batch_size
+            end = (idx + 1) * batch_size
+
+            anchor = self._candidate_vectors[anchor_idxs[start:end], :]
+
+            ramdomly_selected_positive_idx = np.random.randint(0, self._inner_k, (batch_size,))
+            ramdomly_selected_negative_idx = np.random.randint(self._inner_k, self._outer_k, (batch_size,))
+            positive_idxs = self._candidate_self_knn[anchor_idxs[start:end], ramdomly_selected_positive_idx]
+            negative_idxs = self._candidate_self_knn[anchor_idxs[start:end], ramdomly_selected_negative_idx]
             label = label_all_np[start:end]
             label_output = label_all[start:end]
             other_idx = positive_idxs * label + negative_idxs * (1 - label)
