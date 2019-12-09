947758bbeee923c383607661e029e414fff4a68b
stegben <stegben.benjamin@gmail.com>
2019-12-09 21:16:10 +0800

    add CrossEntropy tests

--- nlsh/learning/tests/test_distances.py
+++ nlsh/learning/tests/test_distances.py
@@ -1,7 +1,11 @@
 import numpy as np
 import torch
 
-from ..distances import JSD_categorical, KL_multivariate_bernoulli
+from ..distances import (
+    JSD_categorical,
+    KL_multivariate_bernoulli,
+    cross_entropy_multivariate_bernoulli,
+)
 
 
 def test_JSD_categorical():
@@ -21,4 +25,14 @@ def test_KL_multivariate_bernoulli():
     np.testing.assert_array_almost_equal(
         divergence.numpy(),
         np.array([0.510826, 0.368064, 1.757779, 0., 46.0517]),
+    )
+
+
+def test_cross_entropy_multivariate_bernoulli():
+    p = torch.Tensor([[0.5, 0.5], [0.1, 0.9], [0.1, 0.9], [0.1, 0.9], [0.2, 0.8], [1., 0.]])
+    q = torch.Tensor([[0.1, 0.9], [0.5, 0.5], [0.9, 0.1], [0.1, 0.9], [0.2, 0.8], [0., 1.]])
+    divergence = cross_entropy_multivariate_bernoulli(p, q)
+    np.testing.assert_array_almost_equal(
+        divergence.numpy(),
+        np.array([1.203973, 0.693147, 2.082862, 0.325083, 0.500402, 46.0517]),
     )
