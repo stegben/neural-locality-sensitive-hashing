7b00b46467c14728bbd5b71361b475895e48158d
stegben <stegben.benjamin@gmail.com>
2019-12-30 18:18:06 +0800

    refactor VQVAE

--- nlsh/trainers/vqvae.py
+++ nlsh/trainers/vqvae.py
@@ -4,8 +4,7 @@ import torch
 import torch.nn.functional as F
 import numpy as np
 
-from nlsh.metrics import calculate_recall
-from nlsh.indexer import Indexer
+from .base import Trainer
 
 
 class OnePass:
@@ -30,95 +29,6 @@ class OnePass:
 
             vector = self._candidate_vectors[idxs[start:end], :]
             yield vector
-
-
-class VQVAE:
-
-    def __init__(
-            self,
-            hashing,
-            data,
-            model_save_dir,
-            logger,
-        ):
-        self._hashing = hashing
-        self._data = data
-        self._model_save_dir = model_save_dir
-        self._logger = logger
-
-    def fit(self, K, batch_size=1024, learning_rate=3e-4, test_every_updates=1000):
-        if not self._data.prepared:
-            self._data.load()
-        candidate_vectors = self._data.training
-        validation_data = self._data.testing
-        ground_truth = self._data.ground_truth[:, :K]
-
-        self._candidate_vectors = torch.from_numpy(candidate_vectors)
-        self._candidate_vectors_gpu = torch.from_numpy(candidate_vectors).cuda()
-        self._validation_data = torch.from_numpy(validation_data)
-        self._validation_data_gpu = self._validation_data.cuda()
-
-        dataset = OnePass(self._candidate_vectors_gpu)
-        codebook = torch.nn.Embedding(self._hashing._hash_size, self._data.training.shape[1]).cuda()
-        optimizer = torch.optim.SGD(
-            [p for p in self._hashing.parameters()] + [p for p in codebook.parameters()],
-            # codebook.parameters(),
-            lr=learning_rate,
-            # amsgrad=True,
-        )
-
-        global_step = 0
-        best_recall = 0.
-        for _ in range(300):
-            for sampled_batch in dataset.batch_generator(batch_size, True):
-                global_step += 1
-
-                self._hashing.train_mode(True)
-                optimizer.zero_grad()
-                probs = self._hashing.predict(sampled_batch)
-                probs.retain_grad()
-                codes = StraightThroughCodebookLookup.apply(probs, codebook.weight)
-                codes.retain_grad()
-                dist = F.pairwise_distance(codes, sampled_batch)
-                loss = (dist**2).mean()
-
-                # TODO: potential uniform regularizers
-                # loss += self._lambda1 * torch.mm(anchor, anchor.T).max(1)[0].mean()
-                # loss -= self._lambda1 * torch.log(torch.cdist(anchor, anchor).topk(2, dim=1, largest=False)[0][:,1]).mean()
-
-                self._logger.log("training/loss", loss.data.cpu(), global_step)
-                loss.backward()
-                # import ipdb; ipdb.set_trace()
-                optimizer.step()
-                # if global_step % 1 == 0:
-                if global_step % test_every_updates == 0:
-                    # import ipdb; ipdb.set_trace()
-                    self._hashing.train_mode(False)
-                    # import ipdb; ipdb.set_trace()
-                    indexer = Indexer(
-                        self._hashing,
-                        self._candidate_vectors_gpu,
-                        self._data.distance,
-                    )
-                    n_indexes = len(indexer.index2row)
-                    self._logger.log("test/n_indexes", n_indexes, global_step)
-                    std_index_rows = np.std([len(idxs) for idxs in indexer.index2row.values()])
-                    self._logger.log("test/std_index_rows", std_index_rows, global_step)
-
-                    t1 = time()
-                    result = indexer.query(self._validation_data, self._validation_data_gpu, k=K)
-                    t2 = time()
-                    query_time = t2 - t1
-                    current_recall = calculate_recall(list(ground_truth), result, np.mean)
-
-                    if current_recall > best_recall:
-                        base_name = f"{self._model_save_dir}/{self._logger.run_name}_{global_step}_{current_recall:.4f}"
-                        self._hashing.save(base_name)
-                        best_recall = current_recall
-
-                    self._logger.log("test/recall", current_recall, global_step)
-                    qps = self._validation_data.shape[0] / query_time
-                    self._logger.log("test/qps", qps, global_step)
 
 
 class StraightThroughCodebookLookup(torch.autograd.Function):
@@ -159,3 +69,44 @@ class StraightThroughCodebookLookup(torch.autograd.Function):
         grad_codebook_weight.index_add_(0, idx, grad_output_flatten)
 
         return grad_inputs, grad_codebook_weight
+
+
+class VQVAE(Trainer):
+
+    def __init__(
+            self,
+            hashing,
+            data,
+            model_save_dir,
+            logger,
+        ):
+        self._hashing = hashing
+        self._data = data
+        self._model_save_dir = model_save_dir
+        self._logger = logger
+
+    def _get_dataset(self, vectors, self_knn):
+        """
+        vectors: torch float tensor, (n, d)
+        knn: numpy long array, (n, k), precalculated knn of `vectors`
+        """
+        dataset = OnePass(vectors)
+        return dataset
+
+    def _get_loss(self, batch):
+        probs = self._hashing.predict(batch)
+        probs.retain_grad()
+        codes = StraightThroughCodebookLookup.apply(probs, self.codebook.weight)
+        codes.retain_grad()
+        dist = F.pairwise_distance(codes, batch)
+        loss = (dist**2).mean()
+        return loss
+
+    def _prepare_extra_models(self):
+        self.codebook = torch.nn.Embedding(
+            self._hashing._hash_size,
+            self._data.training.shape[1],
+        ).cuda()
+
+    def _get_extra_models_parameters(self):
+        return [p for p in self.codebook.parameters()]
