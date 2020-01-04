4576a72b33f4d6f467388c3e9659c6b7799884ba
stegben <stegben.benjamin@gmail.com>
2020-01-04 20:19:55 +0800

    add negative sampling method for triplet trainer

--- nlsh/trainers/triplet.py
+++ nlsh/trainers/triplet.py
@@ -1,9 +1,16 @@
 from time import time
+from functools import partial
 
 import torch
 import numpy as np
+from tqdm import tqdm
 
 from .base import Trainer
+
+NSM_RANDOM = "random"
+NSM_NEAREST = "nearest"  # get the closest negative sample
+NSM_HARD = "hard"  # sample from all Dn < max(Dp)
+NSM_SEMI_HARD = "semi-hard"  # sample from Dn < (max(Dp) + margin) & Dn > max(Dp)
 
 
 def triplet_loss(
@@ -19,6 +26,53 @@ def triplet_loss(
     return torch.mean(loss_vec)
 
 
+def batch_execute(batchable, execution, batch_size, concatenator):
+    batch_results = []
+    n = batchable.shape[0]
+    n_batches = n // batch_size
+    with torch.no_grad():
+        for idx in range(n_batches):
+            start = idx * batch_size
+            end = (idx + 1) * batch_size
+            batch = batchable[start:end, :]
+            batch_results.append(execution(batch))
+        last_batch = batchable[n_batches*batch_size:, :]
+        batch_results.append(execution(last_batch))
+    return concatenator(batch_results)
+
+
+def nearest_exclude_positive(vectors, distance_function, positive_indexes):
+    batch_results = []
+
+    batch_size = 8
+    n = vectors.shape[0]
+    n_batches = n // batch_size
+
+    with torch.no_grad():
+        diagnal_idx_for_scatter = torch.arange(batch_size).reshape(batch_size, 1).cuda()
+        for idx in tqdm(range(n_batches)):
+            start = idx * batch_size
+            end = (idx + 1) * batch_size
+            batch = vectors[start:end, :]
+            distances = distance_function(batch, vectors)
+            max_value = distances.max()
+            distances.scatter_(1, positive_indexes[start:end, :], max_value)  # give positive indexes max value so that we don't choose them
+            distances.scatter_(1, diagnal_idx_for_scatter, max_value)  # don't select self
+            nearest_negative_sample_idx = distances.argmin(dim=1)
+            batch_results.append(nearest_negative_sample_idx)
+
+        start = n_batches * batch_size
+        diagnal_idx_for_scatter = torch.arange(n - start + 1).reshape(batch_size, 1).cuda()
+        last_batch = vectors[start:, :]
+        distances = distance_function(last_batch, vectors)
+        distances.scatter_(1, positive_indexes[start:, :], distances.max())
+        distances.scatter_(1, diagnal_idx_for_scatter, distances.max())
+        nearest_negative_sample_idx = distances.argmin(dim=1)
+        batch_results.append(nearest_negative_sample_idx)
+
+    return torch.cat(batch_results)
+
+
 class KNearestNeighborTriplet:
 
     def __init__(
@@ -26,12 +80,19 @@ class KNearestNeighborTriplet:
             candidate_vectors,
             candidate_self_knn,
             k=None,
+            encoder=None,
+            distance_func=None,
+            negative_sampling_method="random",
         ):
         self._candidate_vectors = candidate_vectors
         self._candidate_self_knn = candidate_self_knn
 
         self.k = k or candidate_self_knn.shape[1]
         self.n_candidates = self._candidate_vectors.shape[0]
+
+        self.encoder = encoder
+        self.distance_func = distance_func
+        self.negative_sampling_method = negative_sampling_method
 
     def __len__(self):
         return self.n_candidates
@@ -44,7 +105,17 @@ class KNearestNeighborTriplet:
         else:
             anchor_idxs = torch.arange(len(self)).cuda()
         knn_col_idxs = torch.randint(0, self.k, (len(self),))
-        negative_random_idxs = torch.randint(0, len(self), (len(self),))
+
+        if self.negative_sampling_method == NSM_RANDOM:
+            negative_idxs = torch.randint(0, len(self), (len(self),))
+        elif self.negative_sampling_method == NSM_NEAREST:
+            print("prepare new negative_idxs")
+            candidate_encodings = batch_execute(self._candidate_vectors, self.encoder, 128, partial(torch.cat, dim=0))
+            negative_idxs = nearest_exclude_positive(
+                candidate_encodings,
+                self.distance_func,
+                self._candidate_self_knn[:, :self.k],
+            )
 
         for idx in range(n_batches):
             start = idx * batch_size
@@ -55,8 +126,9 @@ class KNearestNeighborTriplet:
             knn_idxs = self._candidate_self_knn[anchor_idxs[start:end], knn_col_idxs[start:end]]
             positive = self._candidate_vectors[knn_idxs, :]
 
-            negative = self._candidate_vectors[negative_random_idxs[start:end], :]
+            negative = self._candidate_vectors[negative_idxs[start:end], :]
             yield anchor, positive, negative
+
 
 class TripletTrainer(Trainer):
 
@@ -69,6 +141,7 @@ class TripletTrainer(Trainer):
             lambda1=0.001,
             margin=0.1,
             positive_k=None,
+            negative_sampling_method='random',
         ):
         self._hashing = hashing
         self._data = data
@@ -77,6 +150,7 @@ class TripletTrainer(Trainer):
         self._lambda1 = lambda1
         self._margin = margin
         self._positive_k = positive_k
+        self._negative_sampling_method = negative_sampling_method
 
     def _get_dataset(self, vectors, self_knn):
         """
@@ -87,6 +161,9 @@ class TripletTrainer(Trainer):
             vectors,
             self_knn,
             k=self._positive_k,
+            negative_sampling_method=self._negative_sampling_method,
+            distance_func=self._hashing._distance_func.pairwise,
+            encoder=self._hashing._hasher,
         )
         return dataset
 
